def0c297822f32306190c92b60ca888115e90eda
Roberto Mazzarello <rmazzarello@gmail.com>
2024-06-06 18:32:22 -0300

    configuramos el type modules

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Desde index")
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hola')
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log("El servidor se esta ejecutando en el puerto:",PORT)
+})
