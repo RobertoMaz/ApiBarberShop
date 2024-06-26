6e39fc32c3d9f1287705efab6569d28821c6bfa0
Roberto Mazzarello <rmazzarello@gmail.com>
2024-06-26 17:18:34 -0300

    quitamos el undefined en whitelist

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ db()
 app.use(express.json())
 
 
-const whiteList = [process.env.FRONTEND_URL, undefined]
+const whiteList = [process.env.FRONTEND_URL]
 
 const corsOptions = {
     origin: function(origin, callback) {
